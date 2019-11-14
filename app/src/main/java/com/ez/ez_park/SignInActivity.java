5c70323088af74d682f57d4ae15c3b92ac951d73
lizaveta-lv <liashova@sheridancollege.ca>
2019-11-14 13:32:58 -0500

    created sign in and sign up activities, added all basic elements

---
+++ app/src/main/java/com/ez/ez_park/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.ez.ez_park;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
